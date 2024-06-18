59e7e4023f63e124d2663a5363ad6976b8df5998
OCKAPBIHKA <152022576+OCKAPBIHKA@users.noreply.github.com>
2024-06-18 20:58:46 +0300

    make project HW 2

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <p>Fotter</p>
+      <button>sent</button>
+      <button>press</button>
+      <button>reset</button>
+    </div>
+  );
+}
+
+export default App;
